4208872a8e52ac2b1a3430e1fa5239d97cba1b90
burak özkaya <ozkayaburak97@gmail.com>
2023-11-28 15:35:41 +0300

    policy tag removed

--- HBWebApiToken/Controllers/BookController.cs
+++ HBWebApiToken/Controllers/BookController.cs
@@ -8,7 +8,7 @@ using System.Security.Claims;
 
 namespace HBWebApiToken.Controllers
 {
-    [Authorize(policy:)]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BookController : ControllerBase
